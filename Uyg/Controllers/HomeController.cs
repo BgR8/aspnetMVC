c6b49ebd8bc813b5aa23a4f9694b6bcd75bebdcb
BgR8 <bugra.argon@gmail.com>
2019-12-25 21:35:01 +0200

    ASPNET MVC Uygulaması

---
+++ Uyg/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Uyg.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string index()
+        {
+            return "Merhaba Dünya, bu ASP.NET MVC Uygulamasıdır";
+        }
+    }
+}
